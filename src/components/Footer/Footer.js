66fb1d0705392da0fc0414668f51c6e35fca86c9
Alex Saavedra <alexsaavedra2023@u.northwestern.edu>
2024-06-20 12:38:00 -0500

    project description

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,9 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
-      className='link footer__link'
+      
     >
-      Created By Raj Shekhar
+    
     </a>
   </footer>
 )
